680fc0f681fec5cd2b8c5989b4e583ee58042dd5
github-cloudlabsuser-998 <167555987+github-cloudlabsuser-998@users.noreply.github.com>
2024-06-13 17:22:36 +0530

    Update routes.py

--- server/routes.py
+++ server/routes.py
@@ -2,7 +2,7 @@
 from flask import request, render_template, make_response
 
 from server.webapp import flaskapp, cursor
-from server.models import Book
+from server.models import Book 
 
 
 @flaskapp.route('/')
